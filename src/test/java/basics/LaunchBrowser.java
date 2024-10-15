d469ff6889c0d85b2e2d8f608d6d3d9873527fa5
PC <PC@DESKTOP-2VPR9IB>
2024-10-15 02:50:27 -0700

    My First Automation Project

---
+++ src/test/java/basics/LaunchBrowser.java
@@ -0,0 +1,12 @@
+package basics;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("www.google.com");
+		
+	}
+
+}
